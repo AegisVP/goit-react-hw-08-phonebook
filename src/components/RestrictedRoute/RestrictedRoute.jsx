185a89a30b46a103dfa4de63338a251ad44653ef
AegisVP <vlad@pysarenko.com>
2022-10-05 20:55:15 +0200

    fix naming

--- src/components/RestrictedRoute/RestrictedRoute.jsx
+++ src/components/RestrictedRoute/RestrictedRoute.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { selectIsLoggedin } from 'redux/selectors';
 
-export const PrivateRoute = ({ component, redirectTo = '/' }) => {
+export const RestrictedRoute = ({ component, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(selectIsLoggedin);
 
   return isLoggedIn ? component : <Navigate to={redirectTo} />;
